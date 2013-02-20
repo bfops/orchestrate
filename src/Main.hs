8d022a80648a45254c3c6b1f925d0af983675318
Ben Foppa <benjamin.foppa@gmail.com>
2013-02-19 20:19:17 -0500

    Summit 0.10 compatibility

--- src/Main.hs
+++ src/Main.hs
@@ -27,22 +27,21 @@ import Config
 
 import Input
 
--- | Loop with an iterator.. forever
-loop :: Monad m => (a -> m a) -> a -> m a
-loop f x = f x >>= loop f
-
 mswitch :: Functor m2 => (m1 (a, Stream m1 r a) -> m2 (b, Stream m1 r a)) -> Stream m1 r a -> Stream m2 r b
 mswitch f s = Stream $ \r -> mswitch f <$$> f (s $< r)
+
+mstream :: (Functor m, Monad m) => m a -> Stream m () a
+mstream = lift . arr . \m _-> m
 
 -- | Entry point
 main :: SystemIO ()
 main = runIO $ runGLFW displayOpts (0, 0 :: Integer) title $ do
         initOpenGL
         initEvents
-        runMIDI title "128:0" "14:0" $ void $ loop mainLoop
+        runMIDI title "128:0" "14:0" $ iterateM_ (map snd . ($< ()))
                                      $ (inputs <&> (<>) <*> midiInputs)
                                    >>> several (lift $ sendNote <$> harmonies <*> arr (map fromNote))
-                                   >>> lift (arr $ \_-> ioMIDI $ \_-> updateGraphics)
+                                   >>> mstream (ioMIDI $ \_-> updateGraphics >> io (sleep 0.01))
     where
         sendNote h (b, Just note) = traverse_ (iff b startNote stopNote 0 . adjustPitch note) h >> flush
         sendNote _ _ = return ()
@@ -50,16 +49,13 @@ main = runIO $ runGLFW displayOpts (0, 0 :: Integer) title $ do
         adjustPitch note dp = pitch' (fromIntegral . (dp +) . fromIntegral) note
 
 harmonies :: Stream Id (Bool, Input) (Set Int16)
-harmonies = arr Just >>> updater (Id <$$> newInputMap . map fromHarmony) initHarmonies >>> arr (set . keys)
+harmonies = updater (barr $ newInputMap . map fromHarmony) initHarmonies >>> arr (set . keys)
     where
-        initHarmonies = Id $ singleton 0 (1 :: Positive Integer)
+        initHarmonies = singleton 0 (1 :: Positive Integer)
 
         newInputMap (_, Nothing) m = m
         newInputMap (True, Just shift) m = insertWith (+) shift 1 m
         newInputMap (False, Just shift) m = modify (\v -> toPos $ fromPos v - 1) shift m <?> m
-
-mainLoop :: Stream MIDI () () -> MIDI (Stream MIDI () ())
-mainLoop s = (snd <$> s $< ()) <* ioMIDI (\_-> io $ sleep 0.01)
 
 midiInputs :: Stream MIDI () [(Bool, Input)]
 midiInputs = lift $ arr $ \_-> map NoteKey <$$> midiIn
