eb55acab3cdfff401ddf6f76ffc0898973f0b5e4
Ben Foppa <benjamin.foppa@gmail.com>
2013-05-06 23:41:22 -0400

    Record and playback bugfix

--- src/Logic/Memory.hs
+++ src/Logic/Memory.hs
@@ -26,26 +26,13 @@ unzipMap m = let (ks, vs) = unzip $ assocs m
 
 type Song = SplitQueue (Tick, (Maybe Velocity, Note))
 
--- | Indicates whether to toggle play/record state, and which one to toggle
-type Toggle = Maybe Bool
-type TrackUpdate = ((Toggle, Tick), Chord)
+type TrackUpdate = ((Maybe Bool, Tick), Chord)
 type Memory = Map Track (Stream Id TrackUpdate Chord)
 
 edge :: MonadPlus m => (a -> a -> Bool) -> a -> Stream Id (a, m b) (m b)
 edge f = loop $ barr edgeFunc
     where
         edgeFunc (next, out) prev = (guard (f prev next) >> out, next)
-
-toggle :: Num a => Maybe a -> Maybe a
-toggle m = m $> Nothing <?> Just 0
-
-timer :: Num t => (a -> Bool) -> (a -> t) -> Stream Id (a, Maybe t) b -> Stream Id a b
-timer p dt s = loop timerStep Nothing
-    where
-        timerStep = proc (a, t) -> do
-                        let t' = (dt a +) <$> t
-                        b <- s -< (a, t')
-                        id -< (b, if' (p a) toggle t')
 
 -- | If the track does not exist, create it
 createTrack :: Track -> Memory -> Memory
@@ -67,24 +54,28 @@ memory = loop (barr memoryFunc) mempty >>> arr concat
         perTrackInput k = filter (snd >>> (== k)) >>> map fst
 
 track :: Stream Id TrackUpdate Chord
-track = loop (withFstFst record >>> play) emptySplit
+track = map2 (updater (barr state) Nothing)
+    >>> loop (playTime &&& (recordTime >>> record) >>> play) emptySplit
     where
-        withFstFst s = arr (fst >>> fst) &&& s
+        state (new, dt) Nothing = new <&> (, dt)
+        state (new, dt) (Just (prev, t)) = guard (new /= Just prev)
+                                        >> Just (prev, t + dt)
 
-record :: Stream Id (((Toggle, Tick), Chord), Song) Song
-record = timer (fst >>> fst >>> fst >>> (== Just False)) (fst >>> fst >>> snd) $ barr recordFunc
+        playTime = arr $ fst >>> fst >>> filter fst >>> map snd
+        recordTime = arr $ map2 $ map2 $ filter (not . fst) >>> map snd
+
+record :: Stream Id ((Maybe Tick, Chord), Song) Song
+record = barr $ \(t, notes) -> try (append notes) t
     where
-        recordFunc ((_, notes), song) t = try (append notes) t song
         append notes t q = foldr enqSplit q $ (t,) <$> notes
 
-play :: Stream Id ((Toggle, Tick), Song) (Chord, Song)
-play = timer (fst >>> fst >>> (== Just True)) (fst >>> snd)
-     $ proc (a, t) -> do
-            (toPlay, remain) <- barr playFunc -< (a, t)
+play :: Stream Id (Maybe Tick, Song) (Chord, Song)
+play = proc (t, song) -> do
+            (toPlay, remain) <- barr playFunc -< (song, t)
             currentlyPlaying <- mapMaybe hold >>> map held >>> arr last >>> latch mempty >>> arr toList -< toPlay
             -- Release currently playing notes if the track stops
             stop <- edge (>) False -< (t <&> (\_-> True) <?> False, currentlyPlaying)
             id -< (toPlay <> ((Nothing,) <$> stop), remain)
     where
-        playFunc (_, song) t = shiftTrack song <$> t <?> ([], reset song)
+        playFunc song t = shiftTrack song <$> t <?> ([], reset song)
         shiftTrack song t = map2 (map snd) $ shiftWhile (fst >>> (<= t)) song
