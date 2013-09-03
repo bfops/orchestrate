db7cdcc6154edfdc94bc399fd0c186851bc1348c
Ben Foppa <benjamin.foppa@gmail.com>
2013-09-02 23:30:47 -0400

    scripts/issues.sh more flexible about directories

--- scripts/issues.sh
+++ scripts/issues.sh
@@ -2,14 +2,6 @@
 
 cd meristem
 
-if [ -d bugs ]; then
-    for bug in bugs/*; do
-        echo "$bug": `head -1 "$bug"`
-    done
-fi
-
-if [ -d improvements ]; then
-    for i in improvements/*; do
-        echo "$i": `head -1 "$i"`
-    done
-fi
+find -type f | while read file; do
+  echo "$file": `head -1 "$file"`
+done
